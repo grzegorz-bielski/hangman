43c2673b601efd1c9b097891b989c2ba338962b5
Grzegorz Bielski <pesiok@gmail.com>
2019-03-30 22:46:42 +0100

    changed game rules

--- src/Hangman/Game.hs
+++ src/Hangman/Game.hs
@@ -6,8 +6,14 @@ import Data.Maybe (isJust)
 import System.Exit (exitSuccess) 
 import System.IO (hSetBuffering, stdout, BufferMode(NoBuffering))
 
-import Hangman.Puzzle (Puzzle(..), fillInCharacter, freshPuzzle, charInPuzzleWord, alreadyGuessed)
 import Hangman.Words (getRandomWord)
+import Hangman.Puzzle ( Puzzle(..)
+                      , fillInCharacter
+                      , freshPuzzle
+                      , charInPuzzleWord
+                      , alreadyGuessed
+                      , maxAttempts)
+
 
 handleGuess :: Puzzle -> Char -> IO Puzzle
 handleGuess puzzle guess = do 
@@ -15,32 +21,39 @@ handleGuess puzzle guess = do
     let currentState = (charInPuzzleWord puzzle guess, alreadyGuessed puzzle guess)
     case currentState of
         (_, True)  -> putStrLn "You already guessed that character!" >> pure puzzle
-        (True, _)  -> putStrLn "This character was in the word, congrats!" >> pure newPuzzle
-        (False, _) -> putStrLn "This character wasn't in the word!" >> pure newPuzzle
-    where newPuzzle = fillInCharacter puzzle guess
+        (True, _)  -> putStrLn "This character was in the word, congrats!" >> correctGuess
+        (False, _) -> putStrLn "This character wasn't in the word!" >> inCorrectGuess
+    where gameGuess = fillInCharacter puzzle guess
+          correctGuess = pure $ gameGuess 0
+          inCorrectGuess = pure $ gameGuess 1
 
 gameOver :: Puzzle -> IO ()
-gameOver (Puzzle wordToGuess _ guessed) =
+gameOver (Puzzle wordToGuess _ _ attempts) =
     when isGameOver $ do
         putStrLn "You lose!"
         putStrLn ("The word was: " <> wordToGuess) >> exitSuccess
-    where isGameOver = (length guessed) > 7
+    where isGameOver = attempts > maxAttempts
 
 gameWin :: Puzzle -> IO ()
-gameWin (Puzzle _ filledInSoFar _) =
+gameWin (Puzzle _ filledInSoFar _ _) =
     when isGameWon $ putStrLn "You win!" >> exitSuccess
     where isGameWon = all isJust filledInSoFar
+
+clear :: IO ()
+clear = putStr "\ESC[2J"
 
 runGame :: Puzzle -> IO ()
 runGame puzzle = forever $ do
     gameOver puzzle
     gameWin puzzle
+    clear
     putStrLn $ "Current: " <> show puzzle
-    putStr "Guess a letter: "
+    putStr "\nGuess a letter: "
     guess <- getLine
     case guess of
         [c] -> handleGuess puzzle c >>= runGame
         _ -> putStrLn "You may only guess a sinle character"
+
 
 startGame :: IO ()
 startGame = do
--- src/Hangman/Puzzle.hs
+++ src/Hangman/Puzzle.hs
@@ -4,25 +4,38 @@ module Hangman.Puzzle
 , freshPuzzle
 , charInPuzzleWord
 , alreadyGuessed
+, maxAttempts
 ) where
 
 import Data.List (intersperse)
 
-data Puzzle = Puzzle String [Maybe Char] [Char]
+type PuzzleWord = String
+type Discovered = [Maybe Char]
+type Guessed = [Char]
+type Attempts = Int
+
+maxAttempts = 6 :: Int
+
+data Puzzle = Puzzle PuzzleWord Discovered Guessed Attempts
 
 instance Show Puzzle where
-    show (Puzzle _ discovered guessed) = 
-        (intersperse ' ' $ fmap renderPuzzleChar discovered)
-        <> " Guessed so far: " <> guessed
+    show (Puzzle _ discovered guessed attempts) = 
+        (addSpace $ fmap renderPuzzleChar discovered)
+        <> "\n"
+        <> "\nGuessed so far: " <> addSpace guessed
+        <> "\nLives left: " <> show (maxAttempts - attempts)
+
+addSpace :: String -> String
+addSpace = intersperse ' '
 
 freshPuzzle :: String -> Puzzle
-freshPuzzle word = Puzzle word (fmap (const Nothing) word) []
+freshPuzzle word = Puzzle word (fmap (const Nothing) word) [] 0
 
 charInPuzzleWord :: Puzzle -> Char -> Bool
-charInPuzzleWord (Puzzle word _ _) c = c `elem` word
+charInPuzzleWord (Puzzle word _ _ _) c = c `elem` word
 
 alreadyGuessed :: Puzzle -> Char -> Bool
-alreadyGuessed (Puzzle _ guessed _) c = foldr check False guessed
+alreadyGuessed (Puzzle _ guessed _ _ ) c = foldr check False guessed
     where check Nothing acc = acc
           check (Just a) acc = if c == a then True else acc
 
@@ -30,9 +43,9 @@ renderPuzzleChar :: Maybe Char -> Char
 renderPuzzleChar Nothing = '_'
 renderPuzzleChar (Just a) = a
 
-fillInCharacter :: Puzzle -> Char -> Puzzle
-fillInCharacter (Puzzle word filledSoFar s) c =
-    Puzzle word currentFilledSoFar (c : s)
+fillInCharacter :: Puzzle -> Char -> Int -> Puzzle
+fillInCharacter (Puzzle word filledSoFar s a) c attempt =
+    Puzzle word currentFilledSoFar (c : s) (a + attempt)
     where
         zipper :: Char -> Char -> Maybe Char -> Maybe Char
         zipper guessed wordChar guessChar
